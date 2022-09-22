295e0cdc785a8407d83143db60800fab872c864c
zhaohongqiangsoliva <38648009+zhaohongqiangsoliva@users.noreply.github.com>
2022-09-22 13:57:05 +0800

    add extarc

--- newpy.sh
+++ newpy.sh
@@ -4,10 +4,41 @@ usage() { echo -e "Usage: $0 [-s <string>] [-p <string>] \ncreate template for p
 while getopts ":s:p:" o; do
     case "${o}" in
         s)
-            s=${OPTARG}
+                s=${OPTARG}
+                if [ -f "${s}" ]; then
+                echo ${s} '文件已经存在，不能重复创建'
+                else 
+                echo '
+                    tempplate for detial
+                    @Author:zhaohq 
+                    @Email:zhaohongqiangsoliva@gmail.com
+                    Usage:
+                        ${s}.py [--mindp int]
+                        VCFDPFilter.py -h | --help | -e | --example |
+                    Notes:
+                        notes detial 
+                    Options:
+                        -参数 --参数     Explanation
+                    import sys
+                    from docopt import docopt
+                ' >> ${s}.py
+                echo $1 '文件创建成功'
+                fi
             ;;
         p)
             p=${OPTARG}
+            if [ -d "${p}" ]; 
+            then
+                echo "${p} project already exists"
+            else    
+                mkdir -pv ${p} ${p}/{bin,data,Script,note,process/log}
+
+                touch ${p}/process/{main.sh,main.py}
+            fi
+            ;;
+        x)
+            cat Extarc/extarc.sh >> ~/.bashrc
+            
             ;;
         *)
             usage
@@ -19,38 +50,3 @@ shift $((OPTIND-1))
 if [ -z "${s}" ] && [ -z "${p}" ]; then
     usage
 else
-
-if [ -n "${s}" ] ;then
- if [ -f "${s}" ]; then
- echo ${s} '文件已经存在，不能重复创建'
-else 
- echo '
-    tempplate for detial
-    @Author:zhaohq 
-    @Email:zhaohongqiangsoliva@gmail.com
-    Usage:
-        ${s}.py [--mindp int]
-        VCFDPFilter.py -h | --help | -e | --example |
-    Notes:
-        notes detial 
-    Options:
-        -参数 --参数     Explanation
-    import sys
-    from docopt import docopt
-' >> ${s}.py
- echo $1 '文件创建成功'
- fi
-fi
-if [  -n "${p}" ] ;then
-    if [ -d "${p}" ]; 
-    then
-        echo "${p} project already exists"
-    else    
-        mkdir -pv ${p} ${p}/{bin,data,Script,note,process/log}
-
-        touch ${p}/process/{main.sh,main.py}
-    fi
-fi
-
-
-fi
